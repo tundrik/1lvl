16eadbdde394d8a0a0856aea2ac185c20a80da44
tundrik <anokhinem@yandex.ru>
2024-01-30 23:31:54 +0300

    format

--- develop/dev15/main.go
+++ develop/dev15/main.go
@@ -1,17 +1,17 @@
 /*
 *	15) К каким негативным последствиям может привести данный фрагмент кода,
-*   и как это исправить? Приведите корректный пример реализации.
+*	и как это исправить? Приведите корректный пример реализации.
 *
-*   var justString string
+*	var justString string
 *
-*   func someFunc() {
-*       v := createHugeString(1 << 10)
-*       justString = v[:100]
-*   }
+*	func someFunc() {
+*		v := createHugeString(1 << 10)
+*		justString = v[:100]
+*	}
 *
-*   func main() {
-*       someFunc()
-*   }
+*	func main() {
+*		someFunc()
+*	}
 *
 *************************************************/
 package main
