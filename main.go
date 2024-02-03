bd939332a63a1d71726ba6ae0d8a4fc57d993321
tundrik <anokhinem@yandex.ru>
2024-02-04 00:40:27 +0300

    comment slice

--- main.go
+++ main.go
@@ -2,20 +2,25 @@ package main
 
 import "fmt"
 
-func main() {
-	source := []int{0, 1, 2, 3, 4, 5, 6, 7, 8, 9}
-	fmt.Printf("len %d \n", len(source))
-	fmt.Printf("cap %d \n", cap(source))
-	s1 := append(source[:10], 99)
+func someAction(v []int8, b int8) {
+	v[0] = 100
+	v = append(v, b)
+}
 
-	fmt.Printf("len %d \n", len(s1))
-	fmt.Printf("cap %d \n", cap(s1))
-	s1[0] = 77
-	fmt.Printf("source %v \n", source)
-	fmt.Printf("s1 %v \n", s1)
-	//s2 := append(s1, 99) 
-	//fmt.Printf("len %d \n", len(s2))
-	//fmt.Printf("cap %d \n", cap(s2))
-	//fmt.Printf("v %v \n", source)
-	//fmt.Printf("v %v \n", s2)
+func main() {
+	a := make([]int8, 2, 3)
+	fmt.Println(len(a))
+	fmt.Println(cap(a))
+
+    a[0] = 66
+
+	fmt.Println(len(a))
+	fmt.Println(cap(a))
+
+	someAction(a, 6)
+
+	fmt.Println(len(a))
+	fmt.Println(cap(a))
+
+	fmt.Println(a[:3])
 }
